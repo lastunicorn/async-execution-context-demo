04b2786ec46439a55174ce76a8c7ef4a1904500a
Alexandru Iuga <alex.iuga@gmail.com>
2025-08-29 15:28:29 +0300

    added bnet core business

---
+++ sources/ExecutionContextDemo.NetCore.Business/ValuesUseCase.cs
@@ -0,0 +1,18 @@
+﻿namespace ExecutionContextDemo.Controllers;
+
+public class ValuesUseCase
+{
+    public async Task<IEnumerable<string>> Execute()
+    {
+        return await GetValues().ConfigureAwait(false);
+    }
+
+    private Task<IEnumerable<string>> GetValues()
+    {
+        return Task.Run(() =>
+        {
+            string[] strings = new string[] { "value1", "value2" };
+            return strings as IEnumerable<string>;
+        });
+    }
+}
--- sources/ExecutionContextDemo.NetCore/Controllers/WeatherForecastController.cs
+++ sources/ExecutionContextDemo.NetCore/Controllers/WeatherForecastController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 
 namespace ExecutionContextDemo.NetCore.Controllers;
+
 [ApiController]
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
