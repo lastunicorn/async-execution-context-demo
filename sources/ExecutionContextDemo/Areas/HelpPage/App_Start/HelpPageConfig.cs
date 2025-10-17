d8575920aa8b1ea67532e52cc44b2e5c01ab265c
Alexandru Iuga <alex.iuga@gmail.com>
2025-10-17 18:10:24 +0300

    fixed namespaces

--- sources/ExecutionContextDemo/Areas/HelpPage/App_Start/HelpPageConfig.cs
+++ sources/ExecutionContextDemo/Areas/HelpPage/App_Start/HelpPageConfig.cs
@@ -26,7 +26,7 @@ namespace DustInTheWind.ExecutionContextDemo.Areas.HelpPage
     public static class HelpPageConfig
     {
         [SuppressMessage("Microsoft.Globalization", "CA1303:Do not pass literals as localized parameters",
-            MessageId = "ExecutionContextDemo.Areas.HelpPage.TextSample.#ctor(System.String)",
+            MessageId = "DustInTheWind.ExecutionContextDemo.Areas.HelpPage.TextSample.#ctor(System.String)",
             Justification = "End users may choose to merge this string with existing localized resources.")]
         [SuppressMessage("Microsoft.Naming", "CA2204:Literals should be spelled correctly",
             MessageId = "bsonspec",
--- sources/ExecutionContextDemo/Properties/AssemblyInfo.cs
+++ sources/ExecutionContextDemo/Properties/AssemblyInfo.cs
@@ -1,11 +1,10 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ExecutionContextDemo")]
+[assembly: AssemblyTitle("DustInTheWind.ExecutionContextDemo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
